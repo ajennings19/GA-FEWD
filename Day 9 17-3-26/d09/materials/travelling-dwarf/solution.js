a8dc62c5894cd59ab0402eda8e2a66282568c5c4
ajennings19 <amandajennings@Amandas-MacBook-Pro.local>
2017-03-16 21:27:29 -0400

    homework 5

--- Day 9 17-3-26/d09/materials/travelling-dwarf/solution.js
+++ Day 9 17-3-26/d09/materials/travelling-dwarf/solution.js
@@ -1,1 +1,125 @@
 console.log("WRITE YOUR SOLUTION IN HERE!");
+    
+var move = 0;
+
+while (move <= 3) {
+	d.move()
+	move = move + 1
+}
+
+d.orient("down");
+
+var move = 0;
+
+while (move <= 15) {
+	d.move()
+	move = move + 1
+}
+
+d.orient("right");
+
+var move = 0;
+
+while (move <= 5) {
+	d.move()
+	move = move + 1
+}
+
+d.orient("up");
+
+var move = 0;
+
+while (move <= 10) {
+	d.move()
+	move = move + 1
+}
+
+d.orient("right");
+
+var move = 0;
+
+while (move <= 5) {
+	d.move()
+	move = move + 1
+}
+
+d.orient("down");
+
+var move = 0;
+
+while (move <= 8) {
+	d.move()
+	move = move + 1
+}
+
+d.orient("right");
+
+var move = 0;
+
+while (move <= 1) {
+	d.move()
+	move = move + 1
+}
+
+
+
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.orient("down");
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.orient("right");
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.orient("up");
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.orient("right");
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.orient("down");
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.orient("right");
+// d.move();
+// d.move();
